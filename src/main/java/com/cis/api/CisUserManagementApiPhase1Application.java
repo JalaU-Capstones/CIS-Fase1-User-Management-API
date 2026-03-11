3c90354e9c0ff0ce4a29ae22f66ec0067c774d17
Alejandro Botina <alejandro.botina0125@jala.university>
2026-03-11 16:10:18 -0500

    chore: initial project setup with Spring Boot 3.4.x, Java 21, layered architecture and basic config

---
+++ src/main/java/com/cis/api/CisUserManagementApiPhase1Application.java
@@ -0,0 +1,13 @@
+package com.cis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CisUserManagementApiPhase1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CisUserManagementApiPhase1Application.class, args);
+    }
+
+}
