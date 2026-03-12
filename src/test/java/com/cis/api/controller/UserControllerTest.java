fa78584165ddbe3f998ddccfb257f47fe1c1f950
Alejandro Botina <alejandro.botina0125@jala.university>
2026-03-12 17:56:28 -0500

    fix: allow public access to GET /api/v1/users and update tests
    - Configured SecurityFilterChain to permitAll() for GET /api/v1/users
    - Imported SecurityConfig in UserControllerTest to fix 401 errors in tests
    - Disabled CSRF for stateless REST API
    
    Ref: R6

--- src/test/java/com/cis/api/controller/UserControllerTest.java
+++ src/test/java/com/cis/api/controller/UserControllerTest.java
@@ -1,11 +1,13 @@
 package com.cis.api.controller;
 
+import com.cis.api.config.SecurityConfig;
 import com.cis.api.dto.UserResponseDto;
 import com.cis.api.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
+@Import(SecurityConfig.class)
 class UserControllerTest {
 
     @Autowired
