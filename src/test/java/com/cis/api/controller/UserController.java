7f6496d2ab56a3b2d9d24f5a348e0c69aa04547a
Alejandro Botina <alejandro.botina0125@jala.university>
2026-03-12 21:24:03 -0500

    fix: remove mistakenly created test files in wrong location (UserController, UserRepository, UserService were placed in test dir by error)

--- src/test/java/com/cis/api/controller/UserController.java
+++
@@ -1,3 +0,0 @@
-package com.cis.api.controller;
-// This file was created by mistake. Keeping it empty of public classes to avoid conflicts.
-class UserControllerPlaceholder {}
--- src/test/java/com/cis/api/service/UserService.java
+++
@@ -1,3 +0,0 @@
-package com.cis.api.service;
-// This file was created by mistake. Keeping it empty of public classes to avoid conflicts.
-class UserServicePlaceholder {}
